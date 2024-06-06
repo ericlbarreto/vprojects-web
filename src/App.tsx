24352745e91f76206f26e29e2195552044c949c5
Vinícius Seabra <viniciusseabra226@gmail.com>
2024-06-06 15:05:24 -0300

    inicio da tela inicial-colaborador

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+import "./global.css"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Menu from "./assets/menu.svg"
+import Sininho from "./assets/sininho.svg"
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 export default function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='h-screen bg-azulBackground'>
+        <div className='bg-branco flex justify-between m-8'>
+          <div className="flex space-x-2">
+            <button><img src= {Menu} alt="Menu" />
+            </button>
+            <div className="translate-x-6">
+          <p className="text-roxoPrincipal font-bold">V-PROJECTS</p>
+          <p>System</p>
+          </div>
+          </div>
+          <div className="flex space-x-2">
+            <button className="-translate-x-12"><img src={Sininho} alt="Sininho de notificações"/></button>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
       </div>
     </>
   )
