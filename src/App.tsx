5356f5ab78cc0418e04c7778092fe2569c72f737
Vinícius Seabra <viniciusseabra226@gmail.com>
2024-06-06 17:17:00 -0300

    ajeitando estilos header tela inicial

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,16 @@ export default function App() {
     <>
       <div className='h-screen bg-azulBackground'>
         <div className='bg-branco flex justify-between m-8'>
-          <div className="flex space-x-2">
+          <div className="flex space-x-2 mb-8">
             <button><img src= {Menu} alt="Menu" />
             </button>
-            <div className="translate-x-6">
-          <p className="text-roxoPrincipal font-bold">V-PROJECTS</p>
-          <p>System</p>
+            <div className="translate-x-6 text-roxoPrincipal">
+          <p className="font-bold italic">V-PROJECTS</p>
+          <p className="bg-indigo-50 rounded-xl text-center font-extralight h-4 w-20 text-xs translate-x-2.5">S y s t e m</p>
           </div>
           </div>
           <div className="flex space-x-2">
-            <button className="-translate-x-12"><img src={Sininho} alt="Sininho de notificações"/></button>
+            <button className="-translate-x-12 mb-6"><img src={Sininho} alt="Sininho de notificações"/></button>
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
               <AvatarFallback>CN</AvatarFallback>
