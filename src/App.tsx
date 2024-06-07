84de6a40518e3d972bdbc65680d72227a44a7214
Vinícius Seabra <viniciusseabra226@gmail.com>
2024-06-07 14:04:14 -0300

    add borda na header da tela inicial

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ export default function App() {
   return (
     <>
       <div className='h-screen bg-azulBackground'>
-        <div className='bg-branco flex justify-between m-8'>
-          <div className="flex space-x-2 mb-8">
+        <div className='bg-branco flex justify-between border-2 border-slate-200'>
+          <div className="flex space-x-2 m-8">
             <button><img src= {Menu} alt="Menu" />
             </button>
             <div className="translate-x-6 text-roxoPrincipal">
@@ -19,8 +19,8 @@ export default function App() {
           <p className="bg-indigo-50 rounded-xl text-center font-extralight h-4 w-20 text-xs translate-x-2.5">S y s t e m</p>
           </div>
           </div>
-          <div className="flex space-x-2">
-            <button className="-translate-x-12 mb-6"><img src={Sininho} alt="Sininho de notificações"/></button>
+          <div className="flex space-x-2 m-8">
+            <button className="-translate-x-12 translate-y-1 mb-6"><img src={Sininho} alt="Sininho de notificações"/></button>
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
               <AvatarFallback>CN</AvatarFallback>
