9c6763bcd1e7d763e93035338bd713f1de0fffa0
VS-Lucas <ufpelucasv@gmail.com>
2024-06-16 01:44:59 -0300

    feat: add button

--- src/components/tutorial360.tsx
+++ src/components/tutorial360.tsx
@@ -80,6 +80,15 @@ function Tutorial() {
                     </div>
                 </div>
             </div>
+
+            <div className="m-4">
+                <p className="font-medium text-lg">Adicione aqui os colaboradores</p>
+
+                <button className="flex items-center justify-center text-white font-semibold mt-4 bg-roxoPrincipal hover:bg-blue-600 rounded-lg w-72 h-12">
+                    <FaPlus /> <span className="ml-2">Adicionar</span>
+                </button>
+            </div>
+
         </div>
     )
 }
