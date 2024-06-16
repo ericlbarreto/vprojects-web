2f1ce86c6db15d1ad5f8d0de5be23d88708d7c32
VS-Lucas <ufpelucasv@gmail.com>
2024-06-16 02:08:15 -0300

    feat: popover

---
+++ src/components/selectColab.tsx
@@ -0,0 +1,25 @@
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { FaPlus } from "react-icons/fa6";
+
+function SelectColab() {
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <button className="flex items-center justify-center text-white font-semibold mt-4 bg-roxoPrincipal hover:bg-blue-600 rounded-lg w-72 h-12">
+                    <FaPlus /> <span className="ml-2">Adicionar</span>
+                </button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80 m-4">
+                <div>
+                    <p>Listar colaboradores</p>
+                </div>
+            </PopoverContent>
+        </Popover>
+    )
+}
+
+export default SelectColab;
--- src/pages/Avaliacao360.tsx
+++ src/pages/Avaliacao360.tsx
@@ -1,7 +1,6 @@
+import SelectColab from "@/components/selectColab";
 import SubHeaderAv from "@/components/subHeaderAv";
 import Tutorial360 from "@/components/tutorial360";
-import { FaPlus } from "react-icons/fa6";
-
 
 function Avaliacao360() {
     return (
@@ -11,10 +10,7 @@ function Avaliacao360() {
                 <Tutorial360 />
                 <div className="m-4">
                     <p className="font-medium text-lg">Adicione aqui os colaboradores</p>
-
-                    <button className="flex items-center justify-center text-white font-semibold mt-4 bg-roxoPrincipal hover:bg-blue-600 rounded-lg w-72 h-12">
-                        <FaPlus /> <span className="ml-2">Adicionar</span>
-                    </button>
+                    <SelectColab />
                 </div>
             </div>
         </div>
