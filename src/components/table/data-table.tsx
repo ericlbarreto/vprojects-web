cba649fa6d1a7a8410826b85023cde3bfb613dcd
Eric Barreto <ericbarreto521@gmail.com>
2024-06-20 08:45:45 -0300

    feat/homeColaborador: adding sorting columns feature

--- src/components/table/data-table.tsx
+++ src/components/table/data-table.tsx
@@ -3,12 +3,12 @@ import {
   SortingState,
   flexRender,
   getCoreRowModel,
-  getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-} from "@tanstack/react-table"
-import { Pencil1Icon, ChevronRightIcon } from "@radix-ui/react-icons"
-
+  getFilteredRowModel,
+} from "@tanstack/react-table";
+import { Pencil1Icon, ChevronRightIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 import {
   Table,
   TableBody,
@@ -16,38 +16,31 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
+import {
+  Payment,
+  columns as defaultColumns,
+} from "./columns"; // Atualize para o caminho correto do seu arquivo columns
 
-export type Payment = {
-  id: string
-  startDate: string;
-  endDate: string;
-  grade: number
-  status: "em andamento" | "finalizado"
+interface DataTableProps {
+  columns?: ColumnDef<Payment, any>[];
+  data: Payment[];
 }
 
-interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
-}
+export function DataTable({ columns = defaultColumns, data }: DataTableProps) {
+  const [sorting, setSorting] = useState<SortingState>([]);
 
-import { useState } from "react"
-
-export function DataTable<TData extends { status: string }, TValue>({
-  columns,
-  data,
-}: DataTableProps<TData, TValue>) {
-  const [sorting, setSorting] = useState<SortingState>([])
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
     state: {
-      sorting,
+      sorting
     },
-  })
+  });
 
   return (
     <div className="rounded-md">
@@ -55,56 +48,52 @@ export function DataTable<TData extends { status: string }, TValue>({
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                )
-              })}
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              ))}
             </TableRow>
           ))}
         </TableHeader>
         <TableBody>
           {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => {
-              return (
-                <TableRow
-                  key={row.id}
-                  className="relative"
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => {
-                    const isStatusCell = cell.column.id === 'status';
-                    const status = row.original.status;
-                    const isOngoing = status === "em andamento";
-                    return (
-                      <TableCell key={cell.id}>
-                        {isStatusCell ? (
-                          <span className={isOngoing ? 'bg-yellow-100 px-2 py-1 rounded' : 'bg-green-100 px-2 py-1 rounded'}>
-                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                          </span>
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                className="relative"
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => {
+                  const isStatusCell = cell.column.id === 'status';
+                  const status = row.original.status;
+                  const isOngoing = status === "em andamento";
+                  return (
+                    <TableCell key={cell.id}>
+                      {isStatusCell ? (
+                        <span className={isOngoing ? 'bg-yellow-100 px-2 py-1 rounded' : 'bg-green-100 px-2 py-1 rounded'}>
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </span>
+                      ) : (
+                        flexRender(cell.column.columnDef.cell, cell.getContext())
+                      )}
+                      {isStatusCell && (
+                        isOngoing ? (
+                          <Pencil1Icon className="absolute right-12 text-roxoPrincipal top-5 cursor-pointer "/>
                         ) : (
-                          flexRender(cell.column.columnDef.cell, cell.getContext())
-                        )}
-                        {isStatusCell && (
-                          isOngoing ? (
-                            <Pencil1Icon className="absolute right-12 text-roxoPrincipal top-5 cursor-pointer "/>
-                          ) : (
-                            <ChevronRightIcon className="absolute right-12 text-roxoPrincipal top-5 cursor-pointer"/>
-                          )
-                        )}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })
+                          <ChevronRightIcon className="absolute right-12 text-roxoPrincipal top-5 cursor-pointer"/>
+                        )
+                      )}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            ))
           ) : (
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
@@ -115,5 +104,5 @@ export function DataTable<TData extends { status: string }, TValue>({
         </TableBody>
       </Table>
     </div>
-  )
+  );
 }
