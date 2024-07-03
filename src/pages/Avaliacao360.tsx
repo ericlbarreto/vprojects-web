141edf43375e7088af0fd5ce80b023ff78c678a6
VS-Lucas <ufpelucasv@gmail.com>
2024-07-03 00:39:19 -0300

    feat: finish av360

--- src/pages/Avaliacao360.tsx
+++ src/pages/Avaliacao360.tsx
@@ -10,6 +10,7 @@ import { getAllCollaborators, getAv360Data, getCurrentCycle, postAv360 } from "@
 import { CurrentCycle } from "@/interfaces/CurrentCycle";
 import { Oval } from "react-loader-spinner";
 import AtencaoModal from "@/components/atencao";
+import DoneCycle from "@/components/doneCycleModal";
 
 function Avaliacao360() {
     const [availableCollaborators, setAvailableCollaborators] = useState<Collaborator[]>([]);
@@ -19,6 +20,8 @@ function Avaliacao360() {
     const [currentCycle, setCurrentCycle] = useState<CurrentCycle>();
     const [disableAddCollab, setDisableAddCollab] = useState<boolean>(false);
     const [loading, setLoading] = useState(true);
+    const [atencao, setAtencao] = useState(false);
+    const [doneCycle, setDoneCycle] = useState(false);
 
     const { getUserData } = useAuth();
     const user = getUserData();
@@ -48,7 +51,7 @@ function Avaliacao360() {
             setDisableAddCollab(availableCollaborators.length === 0);
         }
     }, [availableCollaborators]);
-    
+
 
     useEffect(() => {
         const fetchAv360Data = async () => {
@@ -60,6 +63,7 @@ function Avaliacao360() {
                         evaluatorId: item.evaluatorId,
                         evaluatedId: item.evaluatedId,
                         cycleId: item.cycleId,
+                        isFinished: item.isFinished,
                         assessment: {
                             idReview: item.PeerReviewScores.id,
                             behavior: item.PeerReviewScores.behavior,
@@ -99,6 +103,7 @@ function Avaliacao360() {
                 evaluatorId: user!.id,
                 evaluatedId: collaborator.id,
                 cycleId: currentCycle!.id,
+                isFinished: false,
                 assessment: {
                     idReview: null,
                     behavior: 0,
@@ -127,10 +132,6 @@ function Avaliacao360() {
         }));
     };
 
-    const handleSubmit = async () => {
-        await postAv360(av360Data, user!.id, currentCycle!.id)
-    };
-
     const handleAv360FieldChange = (collaboratorId: number, field: string, value: any) => {
         setAv360Data(prevState => ({
             ...prevState,
@@ -144,7 +145,36 @@ function Avaliacao360() {
         }));
     };
 
-    const [atencao, setAtencao] = useState(false);
+    const handleAv360StatusChange = (id: number, isFinished: boolean) => {
+        setAv360Data((prevData) => ({
+            ...prevData,
+            [id]: {
+                ...prevData[id],
+                isFinished,
+            },
+        }));
+    };
+
+    const allFieldsFilled = () => {
+        for (const collaborator of [...selectedCollaborators, ...availableCollaborators]) {
+            const av360 = av360Data[collaborator.id];
+            if (
+                !av360 ||
+                !av360.assessment ||
+                av360.assessment.behavior === 0 ||
+                av360.assessment.tecniques === 0 ||
+                av360.assessment.toImprove === "" ||
+                av360.assessment.toPraise === ""
+            ) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const handleSubmitCycle = async (submitted: boolean) => {
+        setDoneCycle(submitted);        
+    };
 
     if (loading) {
         return <div className="h-screen flex justify-center items-center"><Oval color="blue" height="80"
@@ -153,10 +183,22 @@ function Avaliacao360() {
 
     return (
         <div className="h-screen">
-            <SubHeaderAv funcaoSalvar={()=>{}} currentStep={2} atencao={atencao} setAtencao={setAtencao}/>
+            <SubHeaderAv
+                currentStep={2}
+                atencao={atencao}
+                setAtencao={setAtencao}
+                allFieldsFilled={allFieldsFilled()}
+                user={user!}
+                currentCycle={currentCycle}
+                av360Data={av360Data}
+                onSubmitCycle={handleSubmitCycle}
+            />
             <div className="pt-32">
                 <div className="flex justify-center">{atencao && (<AtencaoModal setAtencao={setAtencao} atencao={atencao} path="/autoavaliacao" />)}</div>
-                <div className={atencao ? "opacity-50" : ""}>
+                <div className="flex justify-center">
+                    {doneCycle && <DoneCycle setDoneCycle={setDoneCycle} currentCycle={currentCycle!} av360Data={av360Data} />}
+                </div>
+                <div className={atencao || doneCycle ? "opacity-50" : ""}>
                     <Tutorial360 />
                     <div className="m-4">
                         <p className="font-medium text-lg">Adicione aqui os colaboradores</p>
@@ -169,14 +211,12 @@ function Avaliacao360() {
                                 collaborator={collaborator}
                                 onRemove={handleRemoveCollaborator}
                                 onExpandToggle={toggleExpand}
-                                isExpanded={expandedCollaborators[collaborator.id]}
+                                isExpanded={expandedCollaborators[collaborator.id] || false}
                                 onAv360FieldChange={handleAv360FieldChange}
+                                onAv360StatusChange={handleAv360StatusChange}
                                 av360Data={av360Data}
                             />
                         ))}
-                    </div>
-                    <div className="m-4">
-                        <button onClick={handleSubmit} className="p-2 bg-blue-500 text-white rounded">Enviar Avaliações</button>
                     </div>
                 </div>
             </div>
